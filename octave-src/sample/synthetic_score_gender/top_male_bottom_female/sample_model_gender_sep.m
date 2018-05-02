0b0e21685627a88e3a60bf737a578aa18164add4
Meike Zehlike <meike.zehlike@tu-berlin.de>
2018-05-02 19:32:17 +0200

    redid separated datasets with male on top

--- octave-src/sample/synthetic_score_gender/top_male_bottom_female/sample_model_gender_sep.m
+++ octave-src/sample/synthetic_score_gender/top_male_bottom_female/sample_model_gender_sep.m
@@ -1,9 +1,9 @@
-# Created by Octave 4.2.2, Wed May 02 13:24:48 2018 CEST <mzehlike@cit0566>
+# Created by Octave 4.2.2, Wed May 02 18:14:58 2018 CEST <mzehlike@cit0566>
 # name: omega
 # type: matrix
 # rows: 2
 # columns: 1
- 0.03339087019893085
- -0.01214322722035178
+ 0.03842241186817538
+ -0.01712413998328625
 
 
