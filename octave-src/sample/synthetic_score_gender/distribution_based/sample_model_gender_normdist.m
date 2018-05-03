3f60e587950ecf5c3d1acf0e163904d1c2aa729d
Meike Zehlike <meike.zehlike@tu-berlin.de>
2018-05-03 14:56:59 +0200

    regenerated normally distributed data

--- octave-src/sample/synthetic_score_gender/distribution_based/sample_model_gender_normdist.m
+++ octave-src/sample/synthetic_score_gender/distribution_based/sample_model_gender_normdist.m
@@ -1,9 +1,9 @@
-# Created by Octave 4.2.2, Wed May 02 19:55:55 2018 CEST <mzehlike@cit0566>
+# Created by Octave 4.2.2, Thu May 03 13:22:02 2018 CEST <mzehlike@cit0566>
 # name: omega
 # type: matrix
 # rows: 2
 # columns: 1
- NaN
- NaN
+ 9.879196612571054
+ 3.614670012615124
 
 
