10bcc37780a6146c739ab3adfa7430c0ba1af70e
Meike Zehlike <meike.zehlike@tu-berlin.de>
2018-05-11 17:11:12 +0200

    did synthetic experiments with gamma 500

---
+++ octave-src/sample/synthetic/top_male_bottom_female/GAMMA=250/sample_model_gender_sep.m
@@ -0,0 +1,9 @@
+# Created by Octave 4.2.2, Fri May 11 16:17:32 2018 CEST <mzehlike@cit0566>
+# name: omega
+# type: matrix
+# rows: 2
+# columns: 1
+ 0.01190771759505708
+ -0.3915216327388954
+
+
---
+++ octave-src/sample/synthetic/top_male_bottom_female/GAMMA=500/sample_model_gender_sep.m
@@ -0,0 +1,9 @@
+# Created by Octave 4.2.2, Fri May 11 16:48:12 2018 CEST <mzehlike@cit0566>
+# name: omega
+# type: matrix
+# rows: 2
+# columns: 1
+ -0.128065676327603
+ -0.5233016336613855
+
+
