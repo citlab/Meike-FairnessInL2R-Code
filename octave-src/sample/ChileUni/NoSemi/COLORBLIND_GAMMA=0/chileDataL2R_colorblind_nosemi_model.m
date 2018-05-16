2aa85ad4ae2dbe7ab22a5e40440c2781839eaaa7
Meike Zehlike <meike.zehlike@tu-berlin.de>
2018-05-16 20:56:46 +0200

    colorblind nosemi Chile highschool

--- octave-src/sample/ChileUni/NoSemi/COLORBLIND_GAMMA=0/chileDataL2R_colorblind_nosemi_model.m
+++ octave-src/sample/ChileUni/NoSemi/COLORBLIND_GAMMA=0/chileDataL2R_colorblind_nosemi_model.m
@@ -1,10 +1,11 @@
-# Created by Octave 4.2.2, Wed May 16 20:31:24 2018 CEST <mzehlike@cit0566>
+# Created by Octave 4.2.2, Wed May 16 20:53:38 2018 CEST <mzehlike@cit0566>
 # name: omega
 # type: matrix
-# rows: 3
+# rows: 4
 # columns: 1
- 0.03216951919085708
- 0.2451538101623382
- 0.1525612207780407
+ 0.211643293829432
+ 0.03051699558783157
+ 0.2080364999793258
+ 0.1768565948104423
 
 
