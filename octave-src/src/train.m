3c665c3c56785024808788daf902c41cd1a116ef
Meike Zehlike <meike.zehlike@tu-berlin.de>
2018-05-16 11:58:36 +0200

    failed experiment

--- octave-src/src/train.m
+++ octave-src/src/train.m
@@ -47,8 +47,8 @@ arg_list = argv ();
 %training_file = '../sample/ChileUni/GAMMA=100000/chileDataL2R_gender_train.txt'
 %model_file = '../sample/ChileUni/GAMMA=100000/chileDataL2R_gender_model.m'
 
-training_file = '../sample/ChileUni/GAMMA=5000000/chileDataL2R_gender_train.txt'
-model_file = '../sample/ChileUni/GAMMA=5000000/chileDataL2R_gender_model.m'
+%training_file = '../sample/ChileUni/GAMMA=5000000/chileDataL2R_gender_train.txt'
+%model_file = '../sample/ChileUni/GAMMA=5000000/chileDataL2R_gender_model.m'
 
 % HIGHSCHOOL
 %training_file = '../sample/ChileUni/GAMMA=0/chileDataL2R_highschool_train.txt'
