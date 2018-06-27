78a9197c35e9e7cdf0cc1e61bae40dcc71ae8ab1
Meike Zehlike <meike.zehlike@tu-berlin.de>
2018-06-27 18:24:30 +0200

    created testclass

---
+++ test/test_learning/Test_listwiseCost.py
@@ -0,0 +1,22 @@
+'''
+Created on Jun 27, 2018
+
+@author: mzehlike
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testName(self):
+        pass
+
