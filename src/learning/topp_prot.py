9e47745d4fd0f80d66df131efa2087480fe982ef
Meike Zehlike <meike.zehlike@tu-berlin.de>
2018-06-27 17:36:19 +0200

    added doc to function

--- src/learning/topp_prot.py
+++ src/learning/topp_prot.py
@@ -1,4 +1,12 @@
 import numpy as np
 
-def topp_prot(u,v):
-    return np.exp(u/np.sum(np.exp(v)))
+def topp_prot(u, v):
+    '''
+    given a list of predictions (i.e. scores) what is the probability of being at the top position
+    for group u out of all items
+    example: what is the probability of each female (or male respectively) item (u) to be in position 1
+
+    FIXME: rewrite this such that finding the items per group happens here instead of somewhere else
+    and that we pass only the whole dataset to this function
+    '''
+    return np.exp(u / np.sum(np.exp(v)))
