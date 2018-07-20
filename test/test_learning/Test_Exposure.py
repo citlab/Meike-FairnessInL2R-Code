3ba468d8bb9274f9801542d9175e43fa94ba9ce0
stefaniequach <steffi_quach@yahoo.de>
2018-07-20 08:18:34 +0200

    unit test for find items per query method

--- test/test_learning/Test_Exposure.py
+++ test/test_learning/Test_Exposure.py
@@ -1,10 +1,27 @@
 import unittest
+import numpy as np
+from src.learning import exposure
+
+class TestExposureMethod(unittest.TestCase):
+
+    @classmethod
+    def setUp(self):
+        self._all_items = np.array([5, 6, 7, 8, 2, 9, 10, 4, 3])
+        self._prot_group = np.array([5, 6, 8, 4, 3])
+        self._nprot_group = np.array([7, 1, 2, 9, 10])
+
+    def tearDown(self):
+        pass
+
+    def test_exposure_for_each_group(self):
+        expected_prot = 0.02987333554210711
+        actual_prot = exposure.exposure(self._prot_group, self._all_items)
+        np.testing.assert_equal(expected_prot, actual_prot)
+
+        expected_nprot = 0.25866567263568563
+        actual_nprot = exposure.exposure(self._nprot_group, self._all_items)
+        np.testing.assert_equal(expected_nprot, actual_nprot)
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
 
 
-if __name__ == '__main__':
-    unittest.main()
--- test/test_learning/Test_Find.py
+++ test/test_learning/Test_Find.py
@@ -15,6 +15,7 @@ class TestFindMethods(unittest.TestCase):
     def setUp(self):
         self.__query_ids = np.array([1, 1, 1, 1, 2, 2, 3, 3, 3, 4])
         self.__data = np.matrix('1 0 10; 1 1 9; 1 1 8; 1 0 7; 2 0 6; 2 1 5; 3 0 4; 3 0 3; 3 1 2; 4 1 1')
+        self.__prot_idx = np.matrix('False; True; True; False; False; True; False; False; True; True')
 
 
     @classmethod
@@ -43,11 +44,56 @@ class TestFindMethods(unittest.TestCase):
         actual_5 = find.find_items_per_query(self.__data, self.__query_ids, 5)
         self.assertEqual(expected_5.size, actual_5.size)
 
+    def test_find_group_per_query(self):
+        expected_1 = np.matrix('False; True; True; False')
+        actual_1 = find.find_items_per_query(self.__prot_idx, self.__query_ids, 1)
+        np.testing.assert_array_equal(expected_1, actual_1)
 
     def test_find_items_per_group_per_query(self):
-        self.fail("TODO STEFFI: not yet implemented")
+        expected_1_judgments_per_query = np.matrix('1 0 10; 1 1 9; 1 1 8; 1 0 7')
+        expected_1_protected_items_per_query = np.matrix('1 1 9; 1 1 8')
+        expected_1_nonprotected_items_per_query = np.matrix('1 0 10; 1 0 7')
+        actual_1_judgments_per_query, actual_1_protected_items_per_query, actual_1_nonprotected_items_per_query \
+                = find.find_items_per_group_per_query(self.__data, self.__query_ids, 1, self.__prot_idx)
+        np.testing.assert_array_equal(expected_1_judgments_per_query, actual_1_judgments_per_query)
+        np.testing.assert_array_equal(expected_1_protected_items_per_query, actual_1_protected_items_per_query)
+        np.testing.assert_array_equal(expected_1_nonprotected_items_per_query, actual_1_nonprotected_items_per_query)
 
+        expected_2_judgments_per_query = np.matrix('2 0 6; 2 1 5')
+        expected_2_protected_items_per_query = np.matrix('2 1 5')
+        expected_2_nonprotected_items_per_query = np.matrix('2 0 6')
+        actual_2_judgments_per_query, actual_2_protected_items_per_query, actual_2_nonprotected_items_per_query \
+            = find.find_items_per_group_per_query(self.__data, self.__query_ids, 2, self.__prot_idx)
+        np.testing.assert_array_equal(expected_2_judgments_per_query, actual_2_judgments_per_query)
+        np.testing.assert_array_equal(expected_2_protected_items_per_query, actual_2_protected_items_per_query)
+        np.testing.assert_array_equal(expected_2_nonprotected_items_per_query, actual_2_nonprotected_items_per_query)
 
+        expected_3_judgments_per_query = np.matrix('3 0 4; 3 0 3; 3 1 2')
+        expected_3_protected_items_per_query = np.matrix('3 1 2')
+        expected_3_nonprotected_items_per_query = np.matrix('3 0 4; 3 0 3')
+        actual_3_judgments_per_query, actual_3_protected_items_per_query, actual_3_nonprotected_items_per_query \
+            = find.find_items_per_group_per_query(self.__data, self.__query_ids, 3, self.__prot_idx)
+        np.testing.assert_array_equal(expected_3_judgments_per_query, actual_3_judgments_per_query)
+        np.testing.assert_array_equal(expected_3_protected_items_per_query, actual_3_protected_items_per_query)
+        np.testing.assert_array_equal(expected_3_nonprotected_items_per_query, actual_3_nonprotected_items_per_query)
+
+        expected_4_judgments_per_query = np.matrix('4 1 1')
+        expected_4_protected_items_per_query = np.matrix('4 1 1')
+        expected_4_nonprotected_items_per_query = np.matrix('')
+        actual_4_judgments_per_query, actual_4_protected_items_per_query, actual_4_nonprotected_items_per_query \
+            = find.find_items_per_group_per_query(self.__data, self.__query_ids, 4, self.__prot_idx)
+        np.testing.assert_array_equal(expected_4_judgments_per_query, actual_4_judgments_per_query)
+        np.testing.assert_array_equal(expected_4_protected_items_per_query, actual_4_protected_items_per_query)
+        self.assertEqual(expected_4_nonprotected_items_per_query.size, actual_4_nonprotected_items_per_query.size)
+
+        expected_5_judgments_per_query = np.matrix('')
+        expected_5_protected_items_per_query = np.matrix('')
+        expected_5_nonprotected_items_per_query = np.matrix('')
+        actual_5_judgments_per_query, actual_5_protected_items_per_query, actual_5_nonprotected_items_per_query \
+            = find.find_items_per_group_per_query(self.__data, self.__query_ids, 5, self.__prot_idx)
+        self.assertEqual(expected_5_judgments_per_query.size, actual_5_judgments_per_query.size)
+        self.assertEqual(expected_5_protected_items_per_query.size, actual_5_protected_items_per_query.size)
+        self.assertEqual(expected_5_nonprotected_items_per_query.size, actual_5_nonprotected_items_per_query.size)
 
 
 
