897375997d2894473bee270535c8a37dc2301536
Meike Zehlike <meike.zehlike@tu-berlin.de>
2018-07-23 08:28:58 +0000

    experiments

---
+++ octave-src/sample/LawStudents/gender/GAMMA=0/model.m
@@ -0,0 +1,10 @@
+# Created by Octave 4.0.0, Fri Jul 20 13:12:36 2018 UTC <ubuntu@lsat>
+# name: omega
+# type: matrix
+# rows: 3
+# columns: 1
+ -0.02896447675992337
+ 0.2699491155195178
+ 0.09859932796388068
+
+
---
+++ octave-src/sample/LawStudents/gender/GAMMA=LARGE/model.m
@@ -0,0 +1,10 @@
+# Created by Octave 4.0.0, Fri Jul 20 13:08:13 2018 UTC <ubuntu@lsat>
+# name: omega
+# type: matrix
+# rows: 3
+# columns: 1
+ -0.0002005159742595303
+ 0.2699214375019313
+ 0.09865010923572551
+
+
