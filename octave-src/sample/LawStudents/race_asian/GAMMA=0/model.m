6c279c43caceb5b6839001deddbf0ccb14f01095
Meike Zehlike <meike.zehlike@tu-berlin.de>
2018-07-24 09:20:48 +0000

    various experiments

---
+++ octave-src/sample/LawStudents/race_asian/GAMMA=0/model.m
@@ -0,0 +1,10 @@
+# Created by Octave 4.0.0, Mon Jul 23 14:48:56 2018 UTC <ubuntu@lsat>
+# name: omega
+# type: matrix
+# rows: 3
+# columns: 1
+ -0.2263835693867742
+ 0.163471902837365
+ 0.1324401280273155
+
+
---
+++ octave-src/sample/LawStudents/race_black/GAMMA=0/model.m
@@ -0,0 +1,10 @@
+# Created by Octave 4.0.0, Mon Jul 23 14:49:34 2018 UTC <ubuntu@lsat>
+# name: omega
+# type: matrix
+# rows: 3
+# columns: 1
+ -0.2707407842505246
+ 0.1840740121431875
+ 0.1442629329113807
+
+
