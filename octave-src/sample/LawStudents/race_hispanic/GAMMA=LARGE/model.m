02628d836a5d9402157634a9ab67067fb3dd43e4
Meike Zehlike <meike.zehlike@tu-berlin.de>
2018-07-24 19:22:46 +0000

    did LSAT race large gamma experiment

---
+++ octave-src/sample/LawStudents/race_hispanic/GAMMA=LARGE/model.m
@@ -0,0 +1,10 @@
+# Created by Octave 4.0.0, Tue Jul 24 18:16:48 2018 UTC <ubuntu@trec-big>
+# name: omega
+# type: matrix
+# rows: 3
+# columns: 1
+ 0.1475448910428222
+ 0.1557754135849313
+ 0.1336371158445044
+
+
---
+++ octave-src/sample/LawStudents/race_mexican/GAMMA=LARGE/model.m
@@ -0,0 +1,10 @@
+# Created by Octave 4.0.0, Tue Jul 24 18:10:32 2018 UTC <ubuntu@trec-big>
+# name: omega
+# type: matrix
+# rows: 3
+# columns: 1
+ 0.1853666430924722
+ 0.136608636029987
+ 0.07162400585738442
+
+
