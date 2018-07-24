37a99f349fccf2a1402913f0165ab4872d096c79
Meike Zehlike <meike.zehlike@tu-berlin.de>
2018-07-24 12:50:25 +0000

    lsat gender experiment

--- octave-src/sample/LawStudents/gender/GAMMA=LARGE/model.m
+++ octave-src/sample/LawStudents/gender/GAMMA=LARGE/model.m
@@ -1,10 +1,10 @@
-# Created by Octave 4.0.0, Mon Jul 23 15:44:24 2018 UTC <ubuntu@trec-big>
+# Created by Octave 4.0.0, Tue Jul 24 10:40:40 2018 UTC <ubuntu@lsat>
 # name: omega
 # type: matrix
 # rows: 3
 # columns: 1
- -0.008816211017445548
- 0.1589698802245934
- 0.08573265104307663
+ -0.005889863210337462
+ 0.1589579119521604
+ 0.08572249948997823
 
 
