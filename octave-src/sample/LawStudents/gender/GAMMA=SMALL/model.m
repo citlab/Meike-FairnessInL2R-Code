7945daa7d825d60d66d6ca6630a7fb647fed289c
Meike Zehlike <meike.zehlike@tu-berlin.de>
2018-07-24 18:12:44 +0000

    LSAT race experiments with large gamma

---
+++ octave-src/sample/LawStudents/gender/GAMMA=SMALL/model.m
@@ -0,0 +1,10 @@
+# Created by Octave 4.0.0, Tue Jul 24 17:40:53 2018 UTC <ubuntu@lsat>
+# name: omega
+# type: matrix
+# rows: 3
+# columns: 1
+ -0.04935689531086727
+ 0.2014557356314081
+ 0.1105013221370837
+
+
