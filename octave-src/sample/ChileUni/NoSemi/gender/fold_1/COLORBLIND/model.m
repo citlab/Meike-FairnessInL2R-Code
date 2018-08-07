772b15c03e9c21b0109f54f5fbc447009e028cce
Meike Zehlike <meike.zehlike@tu-berlin.de>
2018-08-07 14:00:36 +0000

    TREC small gamma

---
+++ octave-src/sample/ChileUni/NoSemi/gender/fold_1/COLORBLIND/model.m
@@ -0,0 +1,11 @@
+# Created by Octave 4.0.0, Tue Aug 07 13:39:56 2018 UTC <ubuntu@lsat>
+# name: omega
+# type: matrix
+# rows: 4
+# columns: 1
+ 0.2259861849452286
+ 0.006500844845588957
+ 0.1876803463128436
+ 0.1778918719115695
+
+
