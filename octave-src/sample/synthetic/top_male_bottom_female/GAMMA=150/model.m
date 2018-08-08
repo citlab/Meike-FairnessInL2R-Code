6bbcbbd007335306adff95f7a037c3c0646d31ed
Meike Zehlike <meike.zehlike@tu-berlin.de>
2018-08-08 14:54:44 +0000

    new synthetic training

---
+++ octave-src/sample/synthetic/top_male_bottom_female/GAMMA=150/model.m
@@ -0,0 +1,9 @@
+# Created by Octave 4.0.0, Wed Aug 08 14:40:34 2018 UTC <ubuntu@lsat>
+# name: omega
+# type: matrix
+# rows: 2
+# columns: 1
+ -0.05087214898598411
+ 0.0405680968327092
+
+
---
+++ octave-src/sample/synthetic/top_male_bottom_female/GAMMA=75/model.m
@@ -0,0 +1,9 @@
+# Created by Octave 4.0.0, Wed Aug 08 14:14:56 2018 UTC <ubuntu@lsat>
+# name: omega
+# type: matrix
+# rows: 2
+# columns: 1
+ -0.05133724160053049
+ 0.04177942025321583
+
+
