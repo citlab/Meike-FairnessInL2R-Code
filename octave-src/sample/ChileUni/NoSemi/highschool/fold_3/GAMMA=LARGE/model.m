bae3aceb0902be15172e8876383380c113550ea2
Meike Zehlike <meike.zehlike@tu-berlin.de>
2018-08-08 15:43:13 +0000

    redid synthetic experiment

---
+++ octave-src/sample/ChileUni/NoSemi/highschool/fold_3/GAMMA=LARGE/model.m
@@ -0,0 +1,12 @@
+# Created by Octave 4.0.0, Wed Aug 08 15:28:25 2018 UTC <ubuntu@lsat>
+# name: omega
+# type: matrix
+# rows: 5
+# columns: 1
+ NaN
+ NaN
+ NaN
+ NaN
+ NaN
+
+
--- octave-src/sample/synthetic/top_male_bottom_female/GAMMA=150/model.m
+++ octave-src/sample/synthetic/top_male_bottom_female/GAMMA=150/model.m
@@ -1,9 +1,9 @@
-# Created by Octave 4.0.0, Wed Aug 08 14:40:34 2018 UTC <ubuntu@lsat>
+# Created by Octave 4.0.0, Wed Aug 08 15:19:17 2018 UTC <ubuntu@lsat>
 # name: omega
 # type: matrix
 # rows: 2
 # columns: 1
- -0.05087214898598411
- 0.0405680968327092
+ -0.1401166929312035
+ 0.19943282819851
 
 
