07babe4148b56233e943e7ac88fd933497f2356a
Meike Zehlike <meike.zehlike@tu-berlin.de>
2018-08-09 12:03:21 +0000

    new synthetic experiments

--- octave-src/sample/synthetic/top_male_bottom_female/GAMMA=150/model.m
+++ octave-src/sample/synthetic/top_male_bottom_female/GAMMA=150/model.m
@@ -1,9 +1,9 @@
-# Created by Octave 4.0.0, Wed Aug 08 15:19:17 2018 UTC <ubuntu@lsat>
+# Created by Octave 4.0.0, Thu Aug 09 11:56:58 2018 UTC <ubuntu@lsat>
 # name: omega
 # type: matrix
 # rows: 2
 # columns: 1
- -0.1401166929312035
- 0.19943282819851
+ -0.09085959720828674
+ 0.306312715581564
 
 
--- octave-src/sample/synthetic/top_male_bottom_female/GAMMA=75/model.m
+++ octave-src/sample/synthetic/top_male_bottom_female/GAMMA=75/model.m
@@ -1,9 +1,9 @@
-# Created by Octave 4.0.0, Wed Aug 08 14:14:56 2018 UTC <ubuntu@lsat>
+# Created by Octave 4.0.0, Thu Aug 09 11:53:21 2018 UTC <ubuntu@lsat>
 # name: omega
 # type: matrix
 # rows: 2
 # columns: 1
- -0.05133724160053049
- 0.04177942025321583
+ -0.167450247119932
+ 0.3207142253373603
 
 
