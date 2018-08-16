73ff7d5d65693bc8864ea6812e11b4a2baa22352
Meike Zehlike <meike.zehlike@tu-berlin.de>
2018-08-16 13:22:19 +0000

    predicitons for LSAT gender and race

---
+++ octave-src/sample/LawStudents/race_black/GAMMA=SMALL/model.m
@@ -0,0 +1,10 @@
+# Created by Octave 4.0.0, Thu Aug 16 12:59:16 2018 UTC <ubuntu@trec-big>
+# name: omega
+# type: matrix
+# rows: 3
+# columns: 1
+ 0.00599325898382575
+ 0.2047654282158431
+ 0.1098661412169091
+
+
