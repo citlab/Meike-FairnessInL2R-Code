9fb24da739bc80b832ae27a9b9d8fba685ce206e
Meike Zehlike <meike.zehlike@tu-berlin.de>
2018-08-17 08:50:54 +0000

    TREC big gamma 0 new for each fold

--- octave-src/sample/TREC-BIG/run_all_trainings.sh
+++ octave-src/sample/TREC-BIG/run_all_trainings.sh
@@ -13,15 +13,15 @@ GAMMA_LARGE=200000
 echo ""
 #################################################################################
 
-#FOLD=fold_1
+FOLD=fold_1
 
 #echo "$FOLD COLORBLIND..."
 #cd $PATH_TO_EXECUTABLE_LISTNET
 #./train.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/COLORBLIND/ $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_train.csv $PATH_TO_BIG_TREC_DATASETS/$FOLD/COLORBLIND/model.m
 
 #echo "$FOLD GAMMA=0..."
-#cd $PATH_TO_EXECUTABLE_DELTR 
-#./train.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=0/ $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_train.csv $PATH_TO_BIG_TREC_DATASETS/$EXPERIMENT/$FOLD/GAMMA\=0/model.m 0
+cd $PATH_TO_EXECUTABLE_DELTR 
+./train.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=0/ $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_train.csv $PATH_TO_BIG_TREC_DATASETS/$EXPERIMENT/$FOLD/GAMMA\=0/model.m 0
 
 #echo "$FOLD GAMMA=SMALL..."
 #./train.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=SMALL/ $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_train.csv $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=SMALL/model.m $GAMMA_SMALL
@@ -31,7 +31,7 @@ echo ""
 
 ###################################################################################
 
-#FOLD=fold_2
+FOLD=fold_2
 
 #echo "$FOLD COLORBLIND..."
 #cd $PATH_TO_EXECUTABLE_LISTNET
@@ -39,7 +39,7 @@ echo ""
 
 #echo "$FOLD GAMMA=0..."
 #cd $PATH_TO_EXECUTABLE_DELTR 
-#./train.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=0/ $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_train.csv $PATH_TO_BIG_TREC_DATASETS/$EXPERIMENT/$FOLD/GAMMA\=0/model.m 0
+./train.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=0/ $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_train.csv $PATH_TO_BIG_TREC_DATASETS/$EXPERIMENT/$FOLD/GAMMA\=0/model.m 0
 
 #echo "$FOLD GAMMA=SMALL..."
 #./train.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=SMALL/ $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_train.csv $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=SMALL/model.m $GAMMA_SMALL
@@ -49,7 +49,7 @@ echo ""
 
 #####################################################################################
 
-#FOLD=fold_3
+FOLD=fold_3
 
 #echo "$FOLD COLORBLIND..."
 #cd $PATH_TO_EXECUTABLE_LISTNET
@@ -57,7 +57,7 @@ echo ""
 
 #echo "$FOLD GAMMA=0..."
 #cd $PATH_TO_EXECUTABLE_DELTR 
-#./train.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=0/ $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_train.csv $PATH_TO_BIG_TREC_DATASETS/$EXPERIMENT/$FOLD/GAMMA\=0/model.m 0
+./train.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=0/ $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_train.csv $PATH_TO_BIG_TREC_DATASETS/$EXPERIMENT/$FOLD/GAMMA\=0/model.m 0
 
 #echo "$FOLD GAMMA=SMALL..."
 #./train.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=SMALL/ $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_train.csv $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=SMALL/model.m $GAMMA_SMALL
@@ -67,21 +67,21 @@ echo ""
 
 ######################################################################################
 
-FOLD=fold_4
+#FOLD=fold_4
 
 #echo "$FOLD COLORBLIND..."
 #cd $PATH_TO_EXECUTABLE_LISTNET
 #./train.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/COLORBLIND/ $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_train.csv $PATH_TO_BIG_TREC_DATASETS/$FOLD/COLORBLIND/model.m
 
-echo "$FOLD GAMMA=0..."
-cd $PATH_TO_EXECUTABLE_DELTR 
-./train.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=0/ $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_train.csv $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=0/model.m 0
+#echo "$FOLD GAMMA=0..."
+#cd $PATH_TO_EXECUTABLE_DELTR 
+#./train.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=0/ $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_train.csv $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=0/model.m 0
 
-echo "$FOLD GAMMA=SMALL..."
-./train.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=SMALL/ $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_train.csv $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=SMALL/model.m $GAMMA_SMALL
+#echo "$FOLD GAMMA=SMALL..."
+#./train.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=SMALL/ $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_train.csv $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=SMALL/model.m $GAMMA_SMALL
 
-echo "$FOLD GAMMA=LARGE..."
-./train.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=LARGE/ $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_train.csv $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=LARGE/model.m $GAMMA_LARGE
+#echo "$FOLD GAMMA=LARGE..."
+#./train.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=LARGE/ $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_train.csv $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=LARGE/model.m $GAMMA_LARGE
 
 #######################################################################################
 
