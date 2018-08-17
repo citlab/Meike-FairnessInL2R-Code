91bb7054cbb5c509a48049f1c0c1e20c3e4bfaef
Meike Zehlike <meike.zehlike@tu-berlin.de>
2018-08-17 11:02:16 +0200

    rewrote predictions script for big trec

--- octave-src/sample/TREC-BIG/run_all_predictions.sh
+++ octave-src/sample/TREC-BIG/run_all_predictions.sh
@@ -7,12 +7,6 @@ PATH_TO_EXECUTABLE_DELTR=$GIT_ROOT/octave-src/src
 PATH_TO_EXECUTABLE_LISTNET=$GIT_ROOT/Cao_src/listnet-master/src
 PATH_TO_BIG_TREC_DATASETS=$GIT_ROOT/octave-src/sample/TREC-BIG
 
-##########################################################################################
-# all gender predictions
-##########################################################################################
-
-EXPERIMENT=gender
-
 echo ""
 echo "################################# PREDICTING GENDER NOSEMI #############################################"
 
@@ -20,40 +14,40 @@ FOLD=fold_1
 
 echo "$FOLD predictions..."
 cd $PATH_TO_EXECUTABLE_LISTNET
-./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/chileDataL2R_gender_nosemi_fold1_test.txt $PATH_TO_BIG_TREC_DATASETS/$FOLD/COLORBLIND/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/COLORBLIND/
+./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_test.csv $PATH_TO_BIG_TREC_DATASETS/$FOLD/COLORBLIND/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/COLORBLIND/
 
 cd $PATH_TO_EXECUTABLE_DELTR 
-./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/chileDataL2R_gender_nosemi_fold1_test.txt $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=0/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=0/
+./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_test.csv $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=0/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=0/
 
-./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/chileDataL2R_gender_nosemi_fold1_test.txt $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=SMALL/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=SMALL/
+./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_test.csv $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=SMALL/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=SMALL/
 
-./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/chileDataL2R_gender_nosemi_fold1_test.txt $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=LARGE/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=LARGE/
+./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_test.csv $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=LARGE/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=LARGE/
 
 FOLD=fold_2
 
 echo "$FOLD predictions..."
 cd $PATH_TO_EXECUTABLE_LISTNET
-./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/chileDataL2R_gender_nosemi_fold2_test.txt $PATH_TO_BIG_TREC_DATASETS/$FOLD/COLORBLIND/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/COLORBLIND/
+./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_test.csv $PATH_TO_BIG_TREC_DATASETS/$FOLD/COLORBLIND/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/COLORBLIND/
 
 cd $PATH_TO_EXECUTABLE_DELTR 
-./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/chileDataL2R_gender_nosemi_fold2_test.txt $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=0/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=0/
+./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_test.csv $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=0/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=0/
 
-./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/chileDataL2R_gender_nosemi_fold2_test.txt $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=SMALL/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=SMALL/
+./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_test.csv $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=SMALL/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=SMALL/
 
-./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/chileDataL2R_gender_nosemi_fold2_test.txt $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=LARGE/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=LARGE/
+./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_test.csv $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=LARGE/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=LARGE/
 
 FOLD=fold_3
 
 echo "$FOLD predictions..."
 cd $PATH_TO_EXECUTABLE_LISTNET
-./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/chileDataL2R_gender_nosemi_fold3_test.txt $PATH_TO_BIG_TREC_DATASETS/$FOLD/COLORBLIND/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/COLORBLIND/
+./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_test.csv $PATH_TO_BIG_TREC_DATASETS/$FOLD/COLORBLIND/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/COLORBLIND/
 
 cd $PATH_TO_EXECUTABLE_DELTR 
-./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/chileDataL2R_gender_nosemi_fold3_test.txt $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=0/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=0/
+./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_test.csv $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=0/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=0/
 
-./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/chileDataL2R_gender_nosemi_fold3_test.txt $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=SMALL/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=SMALL/
+./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_test.csv $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=SMALL/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=SMALL/
 
-./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/chileDataL2R_gender_nosemi_fold3_test.txt $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=LARGE/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=LARGE/
+./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_test.csv $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=LARGE/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=LARGE/
 
 
 
@@ -61,104 +55,42 @@ FOLD=fold_4
 
 echo "$FOLD predictions..."
 cd $PATH_TO_EXECUTABLE_LISTNET
-./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/chileDataL2R_gender_nosemi_fold4_test.txt $PATH_TO_BIG_TREC_DATASETS/$FOLD/COLORBLIND/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/COLORBLIND/
+./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_test.csv $PATH_TO_BIG_TREC_DATASETS/$FOLD/COLORBLIND/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/COLORBLIND/
 
 cd $PATH_TO_EXECUTABLE_DELTR 
-./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/chileDataL2R_gender_nosemi_fold4_test.txt $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=0/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=0/
+./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_test.csv $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=0/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=0/
 
-./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/chileDataL2R_gender_nosemi_fold4_test.txt $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=SMALL/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=SMALL/
+./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_test.csv $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=SMALL/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=SMALL/
 
-./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/chileDataL2R_gender_nosemi_fold4_test.txt $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=LARGE/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=LARGE/
+./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_test.csv $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=LARGE/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=LARGE/
 
 
 FOLD=fold_5
 
 echo "$FOLD predictions..."
 cd $PATH_TO_EXECUTABLE_LISTNET
-./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/chileDataL2R_gender_nosemi_fold5_test.txt $PATH_TO_BIG_TREC_DATASETS/$FOLD/COLORBLIND/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/COLORBLIND/
+./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_test.csv $PATH_TO_BIG_TREC_DATASETS/$FOLD/COLORBLIND/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/COLORBLIND/
 
 cd $PATH_TO_EXECUTABLE_DELTR 
-./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/chileDataL2R_gender_nosemi_fold5_test.txt $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=0/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=0/
+./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_test.csv $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=0/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=0/
 
-./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/chileDataL2R_gender_nosemi_fold5_test.txt $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=SMALL/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=SMALL/
+./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_test.csv $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=SMALL/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=SMALL/
 
-./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/chileDataL2R_gender_nosemi_fold5_test.txt $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=LARGE/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=LARGE/
+./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_test.csv $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=LARGE/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=LARGE/
 
 
-
-##########################################################################################
-# all highschool type experiments, no semi-private highschools
-##########################################################################################
-
-EXPERIMENT=highschool
-
-FOLD=fold_1
-
-echo ""
-echo "################################# PREDICTING HIGHSCHOOL NOSEMI #############################################"
+FOLD=fold_6
 
 echo "$FOLD predictions..."
 cd $PATH_TO_EXECUTABLE_LISTNET
-./predict.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/chileDataL2R_highschool_nosemi_fold1_test.txt $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/COLORBLIND/model.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/COLORBLIND/
+./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_test.csv $PATH_TO_BIG_TREC_DATASETS/$FOLD/COLORBLIND/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/COLORBLIND/
 
 cd $PATH_TO_EXECUTABLE_DELTR 
-./predict.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/chileDataL2R_highschool_nosemi_fold1_test.txt $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/GAMMA\=0/model.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/GAMMA\=0/
+./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_test.csv $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=0/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=0/
 
-./predict.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/chileDataL2R_highschool_nosemi_fold1_test.txt $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/GAMMA\=SMALL/model.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/GAMMA\=SMALL/
+./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_test.csv $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=SMALL/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=SMALL/
 
-./predict.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/chileDataL2R_highschool_nosemi_fold1_test.txt $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/GAMMA\=LARGE/model.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/GAMMA\=LARGE/
+./predict.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/features_withListNetFormat_withGender_withZscore_candidateAmount-200_test.csv $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=LARGE/model.m $PATH_TO_BIG_TREC_DATASETS/$FOLD/GAMMA\=LARGE/
 
-FOLD=fold_2
-
-echo "$FOLD predictions..."
-cd $PATH_TO_EXECUTABLE_LISTNET
-./predict.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/chileDataL2R_highschool_nosemi_fold2_test.txt $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/COLORBLIND/model.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/COLORBLIND/
-
-cd $PATH_TO_EXECUTABLE_DELTR 
-./predict.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/chileDataL2R_highschool_nosemi_fold2_test.txt $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/GAMMA\=0/model.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/GAMMA\=0/
-
-./predict.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/chileDataL2R_highschool_nosemi_fold2_test.txt $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/GAMMA\=SMALL/model.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/GAMMA\=SMALL/
-
-./predict.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/chileDataL2R_highschool_nosemi_fold2_test.txt $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/GAMMA\=LARGE/model.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/GAMMA\=LARGE/
-
-
-FOLD=fold_3
-
-echo "$FOLD predictions..."
-cd $PATH_TO_EXECUTABLE_LISTNET
-./predict.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/chileDataL2R_highschool_nosemi_fold3_test.txt $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/COLORBLIND/model.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/COLORBLIND/
-
-cd $PATH_TO_EXECUTABLE_DELTR 
-./predict.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/chileDataL2R_highschool_nosemi_fold3_test.txt $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/GAMMA\=0/model.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/GAMMA\=0/
-
-./predict.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/chileDataL2R_highschool_nosemi_fold3_test.txt $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/GAMMA\=SMALL/model.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/GAMMA\=SMALL/
-
-./predict.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/chileDataL2R_highschool_nosemi_fold3_test.txt $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/GAMMA\=LARGE/model.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/GAMMA\=LARGE/
-
-FOLD=fold_4
-
-echo "$FOLD predictions..."
-cd $PATH_TO_EXECUTABLE_LISTNET
-./predict.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/chileDataL2R_highschool_nosemi_fold4_test.txt $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/COLORBLIND/model.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/COLORBLIND/
-
-cd $PATH_TO_EXECUTABLE_DELTR 
-./predict.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/chileDataL2R_highschool_nosemi_fold4_test.txt $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/GAMMA\=0/model.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/GAMMA\=0/
-
-./predict.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/chileDataL2R_highschool_nosemi_fold4_test.txt $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/GAMMA\=SMALL/model.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/GAMMA\=SMALL/
-
-./predict.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/chileDataL2R_highschool_nosemi_fold4_test.txt $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/GAMMA\=LARGE/model.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/GAMMA\=LARGE/
-
-FOLD=fold_5
-
-echo "$FOLD predictions..."
-cd $PATH_TO_EXECUTABLE_LISTNET
-./predict.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/chileDataL2R_highschool_nosemi_fold5_test.txt $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/COLORBLIND/model.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/COLORBLIND/
-
-cd $PATH_TO_EXECUTABLE_DELTR 
-./predict.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/chileDataL2R_highschool_nosemi_fold5_test.txt $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/GAMMA\=0/model.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/GAMMA\=0/
-
-./predict.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/chileDataL2R_highschool_nosemi_fold5_test.txt $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/GAMMA\=SMALL/model.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/GAMMA\=SMALL/
-
-./predict.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/chileDataL2R_highschool_nosemi_fold5_test.txt $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/GAMMA\=LARGE/model.m $PATH_TO_CHILE_NOSEMI_DATASETS/$EXPERIMENT/$FOLD/GAMMA\=LARGE/
 
 
