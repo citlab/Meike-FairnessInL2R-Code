f85b326b8032dbf55fd8ef14433c4b53f5969384
Meike Zehlike <meike.zehlike@tu-berlin.de>
2019-01-16 21:57:34 +0100

    replotted synthetic experiments such that plot is smaller

--- src/visualization/ranking_results_only_protection_status.py
+++ src/visualization/ranking_results_only_protection_status.py
@@ -9,7 +9,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import pandas as pd
 
-PROT_COL_PRED = 1
+PROT_COL_PRED = 3
 PROT_COL_ORIG = 1
 PROT_ATTR = 1
 
@@ -21,43 +21,44 @@ def plot_rankings(input_file1, input_file2, input_file3, input_file4, output_fil
     mpl.rcParams['pdf.use14corefonts'] = True
     mpl.rcParams['text.usetex'] = True
 
-    f, ax = plt.subplots(figsize=(20, 7))
+    f, ax = plt.subplots(figsize=(20, 3))
 
     # Generate dummy info for plot_ChileDataset handles "h"
 #     red_circle, = plt.plot(0, 1.65, 'ro', ms=14, label="protected")
 #     blue_plus, = plt.plot(0, 1.65, 'b+', ms=16, mew=3, label="non-protected");
-
+    markersize_bullet = 11
+    markersize_plus = 13
     data = pd.read_csv(input_file1, sep=",", header=None)
     for i in range(0, k, step_size):
         if data.iloc[i][PROT_COL_ORIG] == PROT_ATTR:
-            plt.plot(i, 1, 'ro', ms=14);
+            plt.plot(i, 1, 'ro', ms=markersize_bullet);
         else:
-            plt.plot(i, 1, 'b+', ms=16, mew=3);
+            plt.plot(i, 1, 'b+', ms=markersize_plus, mew=3);
 
     data = pd.read_csv(input_file2, sep=",", header=None)
     for i in range(0, k, step_size):
         if data.iloc[i][PROT_COL_PRED] == PROT_ATTR:
-            plt.plot(i, 1.25, 'ro', ms=14);
+            plt.plot(i, 1.25, 'ro', ms=markersize_bullet);
         else:
-            plt.plot(i, 1.25, 'b+', ms=16, mew=3);
+            plt.plot(i, 1.25, 'b+', ms=markersize_plus, mew=3);
 
     data = pd.read_csv(input_file3, sep=",", header=None)
     for i in range(0, k, step_size):
         if data.iloc[i][PROT_COL_PRED] == PROT_ATTR:
-            plt.plot(i, 1.5, 'ro', ms=14);
+            plt.plot(i, 1.5, 'ro', ms=markersize_bullet);
         else:
-            plt.plot(i, 1.5, 'b+', ms=16, mew=3);
+            plt.plot(i, 1.5, 'b+', ms=markersize_plus, mew=3);
 
     data = pd.read_csv(input_file4, sep=",", header=None)
     for i in range(0, k, step_size):
         if data.iloc[i][PROT_COL_PRED] == PROT_ATTR:
-            plt.plot(i, 1.75, 'ro', ms=14);
+            plt.plot(i, 1.75, 'ro', ms=markersize_bullet);
         else:
-            plt.plot(i, 1.75, 'b+', ms=16, mew=3);
+            plt.plot(i, 1.75, 'b+', ms=markersize_plus, mew=3);
 
     plt.gca().invert_yaxis()
     plt.xlabel ("ranking position");
-    plt.yticks(np.arange(1, 2, step=.25), ('Ground\nTruth', 'Normal\nL2R', 'Small\nGamma', 'Large\nGamma'))
+    plt.yticks(np.arange(1, 2, step=.25), ('Ground Truth', 'Normal L2R', 'Small Gamma', 'Large Gamma'))
     # plt.legend([red_circle, blue_plus], ['protected', 'non-protected'], loc='upper right')  # , bbox_to_anchor=(0.73, 0.92))
 
     plt.savefig(output_file, bbox_inches='tight')
